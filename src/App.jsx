8c7302e06ad39dc14f500540efd00865e260cd48
shamseer8877 <alishamseer@gmail.com>
2024-09-16 13:09:21 +0530

    App layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Settings from "./pages/Settings";
 import NewUsers from "./pages/Users";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+import Account from "./pages/Account";
 
 const App = () => {
   return (
@@ -15,13 +17,16 @@ const App = () => {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<NewUsers />} />
+            <Route path="account" element={<Account />} />
+          </Route>
           <Route path="login" element={<Login />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="users" element={<NewUsers />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
