03724b102fcca8690ebc8982cdf9e79fc2671b3a
shamseer8877 <alishamseer@gmail.com>
2024-09-16 18:53:32 +0530

    supabase setup

---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+import supabase from "./supabase";
+
+const getCabins = async () => {
+  const { data, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.log(error);
+    throw new Error("Cabins could not be loaded");
+  }
+  return data;
+};
+
+export default getCabins;
